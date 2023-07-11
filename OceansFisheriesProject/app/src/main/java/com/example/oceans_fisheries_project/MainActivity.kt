ae2025a3d2583b76bd0cd91944e35527dcfb5c61
Junseo11 <0302ser@naver.com>
2023-07-11 18:42:11 +0900

    로그인xml 수정

--- OceansFisheriesProject/app/src/main/java/com/example/oceans_fisheries_project/MainActivity.kt
+++ OceansFisheriesProject/app/src/main/java/com/example/oceans_fisheries_project/MainActivity.kt
@@ -2,15 +2,15 @@ package com.example.oceans_fisheries_project
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.oceans_fisheries_project.databinding.ActivityMainBinding
+import com.example.oceans_fisheries_project.databinding.ActivityLoginBinding
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var binding: ActivityMainBinding
+    lateinit var binding: ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
 
         setContentView(binding.root)
     }
