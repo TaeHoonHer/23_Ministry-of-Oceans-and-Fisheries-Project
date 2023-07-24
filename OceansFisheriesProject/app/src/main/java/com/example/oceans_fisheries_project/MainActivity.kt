fdb609d7b4da006da954419a6438cb1ed82b2278
Junseo11 <0302ser@naver.com>
2023-07-24 14:37:27 +0900

    사이드바 구현

--- OceansFisheriesProject/app/src/main/java/com/example/oceans_fisheries_project/MainActivity.kt
+++ OceansFisheriesProject/app/src/main/java/com/example/oceans_fisheries_project/MainActivity.kt
@@ -2,7 +2,9 @@ package com.example.oceans_fisheries_project
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity
 import android.view.View
+import androidx.core.view.GravityCompat
 import com.example.oceans_fisheries_project.databinding.ActivityLoginBinding
 import com.example.oceans_fisheries_project.databinding.ActivityMainBinding
 
@@ -16,20 +18,15 @@ class MainActivity : AppCompatActivity() {
 
         setContentView(binding.root)
 
-
-
-        supportFragmentManager.beginTransaction()
+        supportFragmentManager.beginTransaction()  // 초기 프래그먼트 설정
             .replace(R.id.frag, fragmentMain())
             .commit()
 
-
-
-        binding.bottomnav.setOnItemSelectedListener { item ->
+        binding.bottomnav.setOnItemSelectedListener { item ->  // 바텀 네비게이션 클릭 이벤트
             when (item.itemId) {
                 R.id.home ->supportFragmentManager.beginTransaction()
                     .replace(R.id.frag, fragmentMain())
                     .commit()
-
                 R.id.mypage -> supportFragmentManager.beginTransaction()
                     .replace(R.id.frag, mypageFragment()).commit()
             }
@@ -37,5 +34,7 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-
+    fun toolbarClick(v:View){
+        binding.layoutDraw.openDrawer(GravityCompat.START)
+    }
 }
